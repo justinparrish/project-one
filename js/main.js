1e9b0ed942641434c5573f497e5e42d903b19dc0
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-07 12:30:30 -0400

    adding String.fromCharCode for letters

--- js/main.js
+++ js/main.js
@@ -3,17 +3,26 @@ console.log('Please Save The Dog!')
 const hangmanInfo = {
     // add alphabet(come up with an easier to list them)
     //use charCode()
-        words: ['Friday','Step Brothers','Pulp Fiction'],
-        hints: ['"Playing with my money... is like playing with my emotions"', '"Did you rub your balls on my drums."', "Aw, man, I shot Marvin in the face..."],
+    letter: String.fromCharCode(66),
+    words: ['Friday', 'Step Brothers', 'Pulp Fiction'],
+    hints: ['"Playing with my money... is like playing with my emotions"', '"Did you rub your balls on my drums."',
+        "Aw, man, I shot Marvin in the face..."],
 
 
 
-} 
+}
 console.log(hangmanInfo.hints[1])
+console.log(letter)
 //create 26 divs to add to letter storage
 //1. create 26 divs and add letter to them
+for (let i = 0; i <= 26; i++) {
+    let letterDiv = $('<div></div>')
+    //2. add the divs to the letter storage
+    $('.letter-storage').append(letterDiv)
+    $(letterDiv).addClass('alpha')
+}
 
-//2. add thh divs to the letter storage
+
 
 //create input inside of play area
 
