0ad7e52140186ec6ebea2b2add5ffe316886674e
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-07 12:08:44 -0400

    began defining things and creating elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 console.log('Please Save The Dog!')
+//create object of letters, words, and hints for words
+const hangmanInfo = {
+    // add alphabet(come up with an easier to list them)
+    //use charCode()
+        words: ['Friday','Step Brothers','Pulp Fiction'],
+        hints: ['"Playing with my money... is like playing with my emotions"', '"Did you rub your balls on my drums."', "Aw, man, I shot Marvin in the face..."],
 
-const letterStorage = ['A', 'B', 'C', 'D', 'E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+
+} 
+console.log(hangmanInfo.hints[1])
+//create 26 divs to add to letter storage
+//1. create 26 divs and add letter to them
+
+//2. add thh divs to the letter storage
+
+//create input inside of play area
+
+
+
+//create function to be able to drag letter to solve a word
+
