ed597c3e967c4e050e42d01045fa986a7f8192a8
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-08 09:28:43 -0400

    letter are shown

--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ const hangmanInfo = {
 
 
 }
-console.log(hangmanInfo.hints[1])
+console.log(hangmanInfo.hints)
 console.log(hangmanInfo.letter)
 
 //create 26 divs to add to letter storage
 //1. create 26 divs
 $(document).ready(function() {
-    let storage = $('<div class="letters"></div>').text('a');
+    let storage = $('<div class="letters"></div>').text('');
     for (let i = 0; i < hangmanInfo.letter.length; i++) {
-        storage = $('<div class="letters"></div>').text([i]);
+        storage = $('<div class="letters"></div>').text(hangmanInfo.letter[i].valueOf());
         $('.letter-box').append(storage);
     }
 });
