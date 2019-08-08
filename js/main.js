87321e867de7230fd4d41537c65000aadc399e0e
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-08 14:41:17 -0400

    fixed new game button from concatenating hints together

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const hangmanInfo = {
     //add words that would be solved
     words: ['Friday', 'Step Brothers', 'Pulp Fiction', 'Superbad','Rocky','Scarface', 'Rush Hour'],
     //add hints to the words that need tto be solved
-    hints: ['"Playing with my money... is like playing with my emotions"', '"Did you rub your balls on my drums."','"Aw, man, I shot Marvin in the face..."','"I am McLovin"','"Where did you get the name,"The Italian Stallion"?"','"SAY HELLO TO MY LITTLE FRIEND!!"', '"Don\'t you ever touch a black man\'s radio, boy! You can do that in China but you can get your ass killed out here, man."']
+    hints: ['"Playing with my money... is like playing with my emotions"', '"Did you rub your balls on my drums."','"Aw, man, I shot Marvin in the face..."','"I am McLovin"','"Where did you get the name,"The Italian Stallion"?"','"SAY HELLO TO MY LITTLE FRIEND!!"', '"Don\'t you ever touch a black man\'s radio, boy! You can do that in China but you can get your 🤬 killed out here, man."']
 }
 console.log(hangmanInfo.hints)
 console.log(hangmanInfo.letter)
@@ -29,19 +29,20 @@ $(document).ready(function() {
 
 //on click of new game button
 $('.new-game').on('click', function newGame() {
+    event.preventDefault()
     //1. randomize hints
     let start = hangmanInfo.hints[Math.floor(Math.random() * hangmanInfo.hints.length)];
 
-    console.log(start)
+    console.log(start);
     //2. once hint is randomized place it inside of hint-display div
-    $('.hint-text').append(start);
-    
+    $('.hint-text').text(start);
+
     
 })
 
 //on click of letter move it to the play-area div
 function clickLetter() {
-    let answer = []
+
 
     
 }
@@ -53,12 +54,15 @@ function resetGameButton() {
 }
 //hint button on click function
 $('.hint').on('click', function hintbutton() {
-    swal('Think hard about movies in the 80s, 90s, and 2000s')  
+    swal('Think hard about movies in the 80s, 90s, and 2000s');  
     
 })
 
+//determine if player won or lose depending on letters and length
 function winOrLose() {
+    if(answer == hangmanInfo.words[i]) {
 
+    }
 }
 
 
