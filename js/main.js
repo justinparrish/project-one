49b901df56c281e3389391a76c2bdbd14283050b
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-09 11:45:07 -0400

    hint and answer corresponds to each other

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 console.log('Please Save The Dog!')
-//create object of letters, words, and hints for words
+//create variables letters, words, and hints for words
 let letter = ['A', 'B', 'C', 'D', 'E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const hangmanInfo = [
     {
@@ -30,68 +30,66 @@ const hangmanInfo = [
         hint: '"Don\'t you ever touch a black man\'s radio, boy! You can do that in China but you can get your 🤬 killed out here, man."',
         answer: 'RUSH HOUR'
     },
-    ]
-console.log(hangmanInfo.hints)
-console.log(hangmanInfo.letter)
-
+];
 
 
 //create 26 divs to add to letter storage
-//1. create 26 divs
 $(document).ready(function() {
     let storage = $('<button class="letters"></button>').text('');
-    //2. iterate through array
     for (let i = 0; i < letter.length; i++) {
-        //3. add letters of array into empty divs
         storage = $('<button class="letters"></button>').text(letter[i].valueOf());
         $('.letter-box').append(storage);
     }
 });
 
-
 //on click of new game button
 $('.new-game').on('click', function newGame() {
     event.preventDefault()
-    //1. randomize hints/and words
+    //randomize hints/and words
     let start = hangmanInfo[Math.floor(Math.random() * hangmanInfo.length)];
-    //let answer = hangmanInfo.words[Math.floor(Math.random() * hangmanInfo.words.length)];
-    
     console.log(start);
-    
+   
     
     //2. once hint is randomized place it inside of hint-display div
-    $('.hint-text').text(start);
-   
-      
+    $('.hint-text').text(start.hint)
+    $('.placement').text(start.answer)
 })
-
-
 
 //on click of letter move it to the play-area div
 function clickLetter() {
-
+    let letters = $('.letters')
+    //if letter is selected add it to the answer in placement
+    if()
 
     
 }
 //on click of reset-play-area button
 function resetGameButton() {
-    let reset = $('.reset-play-area');
+    //turns selected letters white
+    let reset = $('.placement');
 
 
 }
+
+//determine if player won or lose depending on letters and length
+function winOrLose() {
+    //if user has correct answer
+    if() {
+        //add 1 point for win
+    }
+    else {
+        //add 1 point to loss
+    }
+}
+
+
+
+
 //hint button on click function
 $('.hint').on('click', function hintbutton() {
     swal('Think hard about movies in the 80s, 90s, and 2000s');  
     
 })
-
-//determine if player won or lose depending on letters and length
-function winOrLose() {
-    if(answer == hangmanInfo.words[i]) {
-
-    }
-}
-
 
 
 
