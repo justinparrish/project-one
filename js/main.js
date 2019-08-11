118c70a3d4d58c5086a4538f6a2e78bb08cbfc1a
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-08-11 11:30:59 -0400

    refreshing the code again

--- js/main.js
+++ js/main.js
@@ -38,13 +38,22 @@ $('.hint-text').text(start.hint)
 console.log(start.answer.length)
 let luckyGuess = []
 let hintAnswer = $('.placement').text(start.answer)
-let lives = 6
-let winScore = Number.parseInt($('.win-score').text())
-let loseScore = Number.parseInt($('.lose-score').text())
+let lives = Number.parseFloat($('.lives').text())
+let winScore = Number.parseFloat($('.win-score').text())
+let loseScore = Number.parseFloat($('.lose-score').text())
 let alphaKeys = $('.letters')
 let numberOfClicks = 0
-console.log(Number.parseInt($('.win-score').text()))
-console.log(Number.parseInt($('.lose-score').text()))
+console.log(Number.parseFloat($('.win-score').text()))
+console.log(Number.parseFloat($('.lose-score').text()))
+console.log(Number.parseFloat($('.lives').text()))
+
+
+//create 26 divs to add to letter storage
+// let storage = $('<button class="letters"></button>').text('');
+for (let i = 0; i < letter.length; i++) {
+    storage = $('<button class="letters"></button>').text(letter[i].valueOf());
+    $('.letter-box').append(storage);
+}
 
 
 for(let i = 0; i < start.answer.length; i++) {
@@ -58,18 +67,28 @@ for(let i = 0; i < start.answer.length; i++) {
 console.log(letter[0].valueOf())
 console.log(hangmanInfo[0].hint.valueOf())
 
-//create 26 divs to add to letter storage
-// let storage = $('<button class="letters"></button>').text('');
-for (let i = 0; i < letter.length; i++) {
-    storage = $('<button class="letters"></button>').text(letter[i].valueOf());
-    $('.letter-box').append(storage);
+
+
+//determine if player won or lose depending on letters and length
+function winOrLose() {
+        //if user has correct answer
+    if( luckyGuess == hintAnswer) {
+        //add 1 point for win
+        winScore + 1;
+        swal('Congratulations!! You Won!!');
+    }
+    else if(luckyGuess > 6){
+        //add 1 point to loss
+        loseScore + 1;
+        swal('Awww you lose🙁...Better Luck Next Time');
+    }     
+
 }
 
+//test for letter buttons
 $('.letters').on('click', function clickedLetter() {
     swal('letter clicked')
 })
-
-
 
 //on click of new game button
 $('.new-game').on('click', function newGame() {
@@ -85,22 +104,6 @@ $('.new-game').on('click', function newGame() {
 })
 
 
-
-//determine if player won or lose depending on letters and length
-function winOrLose() {
-        //if user has correct answer
-    if( hintAnswer == luckyGuess){
-        //add 1 point for win
-        winScore + 1;
-        swal('Congratulations!! You Won!!');
-    }
-    else if(luckyGuess > 6){
-        //add 1 point to loss
-        loseScore + 1;
-        swal('Awww you lose🙁...Better Luck Next Time');
-    }     
-
-}
 //hint button on click function
 $('.hint').on('click', function hintbutton() {
     (swal('Think hard about movies in the 80s, 90s, and 2000s'));  
